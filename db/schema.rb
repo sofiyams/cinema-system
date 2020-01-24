72f5c75b25f181fb80e919508487cac001092183
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-01-24 17:07:42 +0000

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117182657) do
+ActiveRecord::Schema.define(version: 20200124170057) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20200117182657) do
     t.string "language"
     t.decimal "duration"
     t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
