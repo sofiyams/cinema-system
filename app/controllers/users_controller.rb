952355b24b90d2ebb2a0d30d27479392a9b1aa05
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-02-05 20:01:49 +0000

    add sign up upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the De Montfort Cinema #{@user.username}"
-      redirect_to movies_path
+      redirect_to users_path(@user)
     else
       render 'new'
     end
